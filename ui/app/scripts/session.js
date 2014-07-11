f53a0011e2717212728a1843d1e88602cd76801f
mthubert <mehdi.thubert@4sh.fr>
2014-07-11 10:13:40 +0200

    Fix bug with login page

--- ui/app/scripts/session.js
+++ ui/app/scripts/session.js
@@ -1,52 +1,77 @@
 'use strict';
 
-angular
-    .module('rxSession', [
+angular.module('rxSession', [
         'ngResource',
         'angular-md5'
     ])
-    .factory('Sessions', function($resource) {
-        return $resource('/api/sessions/:sessionKey', {sessionKey: 'current'});
+
+    .config(function($httpProvider) {
+        $httpProvider.responseInterceptors.push('SecurityHttpInterceptor');
     })
+
     .factory('SecurityHttpInterceptor', function($q, $location) {
         return function(promise) {
             return promise.then(function(response) {
                 return response;
             }, function(response) {
                 if (response.status == 401 || response.status == 403) {
-                    var backTo = encodeURIComponent(location);
                     $location.path('/login');
                 }
                 return $q.reject(response);
             });
         }
     })
-    .controller('SessionController', function($scope, Sessions) {
-        $scope.session = Sessions.get();
-
-        $scope.logout = function() {
-            Sessions.delete(function() { location.reload() });
-        }
-    })
-    .config(function($httpProvider) {
-        $httpProvider.responseInterceptors.push('SecurityHttpInterceptor');
-    })
-.controller('LoginCtrl', function ($scope, $http, $location, md5, Sessions) {
-        $scope.currentUser = null;
-        $scope.session = Sessions.get(function(data) {
-            $scope.currentUser = data.principal;
+    .factory('Sessions', function($resource, $http, md5) {
+        var res = $resource('/api/sessions/:sessionKey', {sessionKey: 'current'});
+        var currentUser = null;
+        res.get(function(data) {
+            currentUser = data;
         });
-        $scope.authenticate = function(username, password) {
-            $http.post('/api/sessions', {principal: {name: username, passwordHash: md5.createHash(password)}})
-                .success(function(data, status, headers, config) {
-                    console.log('authenticated', data, status);
-                    $scope.currentUser = data.principal;
-                    $location.path('/');
-                }).error(function(data, status, headers, config) {
-                    console.log('error', data, status);
-                    $scope.currentUser = null;
-                    alert("Authentication error, please try again.");
+        return angular.extend(res, {
+            getCurrentUser: function() {
+                return currentUser ? currentUser.principal : null;
+            },
+            authenticate: function(username, password, callback, callbackError) {
+                $http.post('/api/sessions', {principal: {name: username, passwordHash: md5.createHash(password)}})
+                    .success(function(data, status, headers, config) {
+                        currentUser = data;
+                        if (callback) {
+                            callback();
+                        }
+                    }).error(function(data, status, headers, config) {
+                        currentUser = null;
+                        if (callbackError) {
+                            callbackError();
+                        }
+                    });
+            },
+            logout: function() {
+                res.delete(function() {
+                    location.reload();
+                    currentUser = null;
                 });
+            }
+        });
+    })
+
+    .controller('SessionController', function($scope, Sessions) {
+        $scope.session = Sessions;
+        $scope.openPasswordPopup = function() {
+            alertify.log('TODO : Not implemented');
+        };
+    })
+    .controller('LoginCtrl', function ($scope, $location, Sessions, i18nUtils) {
+        $scope.authenticate = function(username, password) {
+            Sessions.authenticate(
+                username ? username : angular.element('#login-username').val(),
+                password ? password : angular.element('#login-password').val(),
+                function() {
+                    $location.url('/');
+                    alertify.success(i18nUtils.translate('login.authenticate.success'));
+                }, function() {
+                    alertify.error(i18nUtils.translate('login.authenticate.error'));
+                }
+            );
         }
     })
 ;
