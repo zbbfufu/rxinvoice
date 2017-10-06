ebd0a23031abbbe76415670aa7bc8866c8e724f4
estellesifre <estelle.sifre@4sh.fr>
2017-10-06 15:40:57 +0200

    new skin page

---
+++ ui/src/app/pages/customer-new/customer-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'customer-new',
+  templateUrl: './customer-new.component.html',
+  styleUrls: ['./customer-new.component.scss']
+})
+export class CustomerNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
