e28d741aedf8f59de81938e796d4007f4c74e6e3
estellesifre <estelle.sifre@4sh.fr>
2017-10-27 18:18:49 +0200

    add page new invoice

---
+++ ui/src/app/pages/invoice-new/invoice-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'invoice-new',
+  templateUrl: './invoice-new.component.html',
+  styleUrls: ['./invoice-new.component.scss']
+})
+export class InvoiceNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
