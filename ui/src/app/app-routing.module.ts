12c6ce06e03d7ae0ab1622c7da843e6f6858edd4
cDaffe <celine.daffe@4sh.fr>
2018-01-12 16:50:26 +0100

    [front] Change customer routing loading costumers component

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { InvoicesListComponent } from './pages/invoices-list/invoices-list.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import {CustomersListComponent} from "./pages/customers-list/customers-list.component";
-import {CustomerNewComponent} from "./pages/customer-new/customer-new.component";
-import {InvoiceNewComponent} from "./pages/invoice-new/invoice-new.component";
+import {CustomerNewComponent} from './pages/customer-new/customer-new.component';
+import {InvoiceNewComponent} from './pages/invoice-new/invoice-new.component';
+import {CustomersComponent} from './pages/customers/customers.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'invoices', component: InvoicesListComponent },
     { path: 'invoices/new', component: InvoiceNewComponent },
-    { path: 'customers', component: CustomersListComponent},
+    { path: 'customers', component: CustomersComponent},
     { path: 'customers/new', component: CustomerNewComponent},
     { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
