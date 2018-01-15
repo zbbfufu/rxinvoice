e2e447dce94092b7d1656fe5042c4dee56583dc2
cDaffe <celine.daffe@4sh.fr>
2018-01-15 15:06:47 +0100

    [front] Add login component

---
+++ ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/authentication.service';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+    constructor(private router: Router,
+                private authService: AuthenticationService) {}
+
+    ngOnInit() {
+
+    }
+
+    authenticate(login) {
+        this.authService.authenticate(login.login, login.password)
+            .subscribe(
+                () => this.router.navigate(['/dashboard']),
+            );
+    }
+
+}
