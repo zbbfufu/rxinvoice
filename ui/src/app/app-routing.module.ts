09577c2de6a4b1f031eda0c4e419e12ba3eec460
cDaffe <celine.daffe@4sh.fr>
2018-01-15 15:09:57 +0100

    [front] add ts-md5 dependency and update app-module and routing

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -5,15 +5,26 @@ import { NgModule } from '@angular/core';
 import {CustomerNewComponent} from './pages/customer-new/customer-new.component';
 import {InvoiceNewComponent} from './pages/invoice-new/invoice-new.component';
 import {CustomersComponent} from './pages/customers/customers.component';
+import {LoginComponent} from './pages/login/login.component';
+import {LoggedInGuard} from './guards/logged-in.guard';
+import {AppContentComponent} from './app-content/app-content.component';
+import {AuthenticationService} from './services/authentication.service';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'invoices', component: InvoicesListComponent },
-    { path: 'invoices/new', component: InvoiceNewComponent },
-    { path: 'customers', component: CustomersComponent},
-    { path: 'customers/new', component: CustomerNewComponent},
-    { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', pathMatch: 'full', component: LoginComponent },
+    {
+        path: '',
+        component: AppContentComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'invoices', component: InvoicesListComponent },
+            { path: 'invoices/new', component: InvoiceNewComponent },
+            { path: 'customers', component: CustomersComponent},
+            { path: 'customers/new', component: CustomerNewComponent},
+            { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
+        ]
+    }
 ];
 
 @NgModule({
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { InvoicesListComponent } from './pages/invoices-list/invoices-list.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
-import { CustomersListComponent } from './pages/customers-list/customers-list.component';
+import { CustomersListComponent } from './components/customers-list/customers-list.component';
 import { CustomerNewComponent } from './pages/customer-new/customer-new.component';
 import { InvoiceNewComponent } from './pages/invoice-new/invoice-new.component';
 import { RepositoryService } from './services/repository.service';
@@ -19,6 +19,11 @@ import { HttpInterceptorService } from './services/http-interceptor.service';
 import {CommonModule} from '@angular/common';
 import { CustomersComponent } from './pages/customers/customers.component';
 import {CompanyService} from './services/company.service';
+import { LoginComponent } from './pages/login/login.component';
+import {HttpModule} from '@angular/http';
+import {LoggedInGuard} from './guards/logged-in.guard';
+import { AppContentComponent } from './app-content/app-content.component';
+import {AuthenticationService} from './services/authentication.service';
 
 export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, '/api/i18n/', 'labels.json');
@@ -35,6 +40,8 @@ export function createTranslateLoader(http: HttpClient) {
         CustomerNewComponent,
         InvoiceNewComponent,
         CustomersComponent,
+        LoginComponent,
+        AppContentComponent,
     ],
     imports: [
         AppRoutingModule,
@@ -55,6 +62,8 @@ export function createTranslateLoader(http: HttpClient) {
         TranslateService,
         RepositoryService,
         CompanyService,
+        LoggedInGuard,
+        AuthenticationService,
         {
             provide: HTTP_INTERCEPTORS,
             useClass: HttpInterceptorService,
