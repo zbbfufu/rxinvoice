af1edc4b28f4215afb4245e4f2df93b43fd91b84
cDaffe <celine.daffe@4sh.fr>
2018-01-15 17:53:02 +0100

    [front] add toggleFilter

--- ui/src/app/pages/customers/customers.component.ts
+++ ui/src/app/pages/customers/customers.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CompanyService} from '../../services/company.service';
 import {CompanyModel} from '../../models/company.model';
 
 @Component({
-  selector: 'customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.scss']
+    selector: 'customers',
+    templateUrl: './customers.component.html',
+    styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
 
     companies: CompanyModel[];
+    filterString: string;
 
 
-  constructor(private companyService: CompanyService) { }
+    constructor(private companyService: CompanyService) {
+    }
 
-  ngOnInit() {
-    this.companyService.fetchCompanies().subscribe((companies) => this.companies = companies);
-  }
+    ngOnInit() {
+        this.companyService.fetchCompanies().subscribe((companies) => this.companies = companies);
+    }
 
+    toggleFilter(string) {
+        if (this.filterString && this.filterString === string) {
+            this.filterString = undefined;
+        } else {
+            this.filterString = string;
+        }
+    }
 }
