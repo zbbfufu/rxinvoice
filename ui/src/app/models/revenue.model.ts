ca109d2bd25f33d59c5dd526d6bc6389789b3ec2
cDaffe <celine.daffe@4sh.fr>
2018-01-16 11:41:54 +0100

    [front] add activity-type, revenue and user-info models

---
+++ ui/src/app/models/revenue.model.ts
@@ -0,0 +1,11 @@
+
+export class RevenueModel {
+    from: Date;
+    to: Date;
+    periodType: string;
+    invoiced: number;
+    paid: number;
+
+  constructor() {
+  }
+}
---
+++ ui/src/app/models/user-info.model.ts
@@ -0,0 +1,7 @@
+export class UserInfoModel {
+    ref: string;
+    name: string;
+
+  constructor() {
+  }
+}
