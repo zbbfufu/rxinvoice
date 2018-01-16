7a8f4ace2e0f6155f4ccfd9123cc94826c8874b1
cDaffe <celine.daffe@4sh.fr>
2018-01-16 11:49:33 +0100

    [front] Update module and routing

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { InvoicesListComponent } from './pages/invoices-list/invoices-list.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {CustomerNewComponent} from './pages/customer-new/customer-new.component';
 import {InvoiceNewComponent} from './pages/invoice-new/invoice-new.component';
 import {CustomersComponent} from './pages/customers/customers.component';
 import {LoginComponent} from './pages/login/login.component';
-import {LoggedInGuard} from './guards/logged-in.guard';
 import {AppContentComponent} from './app-content/app-content.component';
-import {AuthenticationService} from './services/authentication.service';
+import {InvoicesComponent} from './pages/invoices/invoices.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -18,7 +16,7 @@ const routes: Routes = [
         component: AppContentComponent,
         children: [
             { path: 'dashboard', component: DashboardComponent },
-            { path: 'invoices', component: InvoicesListComponent },
+            { path: 'invoices', component: InvoicesComponent },
             { path: 'invoices/new', component: InvoiceNewComponent },
             { path: 'customers', component: CustomersComponent},
             { path: 'customers/new', component: CustomerNewComponent},
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,10 +5,8 @@ import {NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app/app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { InvoicesListComponent } from './pages/invoices-list/invoices-list.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
-import { CustomersListComponent } from './components/customers-list/customers-list.component';
 import { CustomerNewComponent } from './pages/customer-new/customer-new.component';
 import { InvoiceNewComponent } from './pages/invoice-new/invoice-new.component';
 import { RepositoryService } from './services/repository.service';
@@ -20,10 +18,12 @@ import {CommonModule} from '@angular/common';
 import { CustomersComponent } from './pages/customers/customers.component';
 import {CompanyService} from './services/company.service';
 import { LoginComponent } from './pages/login/login.component';
-import {HttpModule} from '@angular/http';
 import {LoggedInGuard} from './guards/logged-in.guard';
 import { AppContentComponent } from './app-content/app-content.component';
 import {AuthenticationService} from './services/authentication.service';
+import {InvoicesComponent} from './pages/invoices/invoices.component';
+import {InvoicesListComponent} from './components/invoices-list/invoices-list.component';
+import {CustomerLineComponent} from './components/customer-line/customer-line.component';
 
 export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, '/api/i18n/', 'labels.json');
@@ -36,12 +36,13 @@ export function createTranslateLoader(http: HttpClient) {
         InvoicesListComponent,
         SidebarComponent,
         AppHeaderComponent,
-        CustomersListComponent,
+        CustomerLineComponent,
         CustomerNewComponent,
         InvoiceNewComponent,
         CustomersComponent,
         LoginComponent,
         AppContentComponent,
+        InvoicesComponent
     ],
     imports: [
         AppRoutingModule,
