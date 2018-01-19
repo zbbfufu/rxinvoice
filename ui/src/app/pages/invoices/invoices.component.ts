8f487b7211d87072ce997ad306769b7ce0a09b93
cDaffe <celine.daffe@4sh.fr>
2018-01-19 17:11:05 +0100

    [front] Update invoices, add research and update display

--- ui/src/app/pages/invoices/invoices.component.ts
+++ ui/src/app/pages/invoices/invoices.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {InvoiceStatusType} from '../../models/invoice-status.type';
 import {InvoiceKindType} from '../../models/invoice-kind.type';
-import {Invoice} from '../../models/invoice.model';
+import {InvoiceModel} from '../../models/invoice.model';
 import {InvoiceService} from '../../common/services/invoice.service';
 import {RepositoryService} from '../../common/services/repository.service';
+import {CompanyModel} from '../../models/company.model';
+import {CompanyService} from '../../common/services/company.service';
+import {FormGroup, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'invoices',
@@ -12,18 +15,25 @@ import {RepositoryService} from '../../common/services/repository.service';
 })
 export class InvoicesComponent implements OnInit {
 
-    invoices: Invoice[];
+    invoices: InvoiceModel[];
     statuses: InvoiceStatusType[];
     kinds: InvoiceKindType[];
-    filterString: string;
+    companies: CompanyModel[];
+    filterString = 'REFERENCE';
+    isPending = false;
+    @ViewChild('searchForm') searchForm: FormGroup;
 
   constructor(private invoiceService: InvoiceService,
+              private companyService: CompanyService,
               private repositoryService: RepositoryService) { }
 
     ngOnInit() {
-        this.invoices = this.invoiceService.fetchInvoices();
-        this.statuses = this.repositoryService.fetchInvoiceStatus();
+       this.research();
+        this.repositoryService.fetchInvoiceStatus()
+            .subscribe(statuses => this.statuses = statuses);
         this.kinds = this.repositoryService.fetchInvoiceKind();
+        this.companyService.fetchCompanies()
+            .subscribe(companies => this.companies = companies);
     }
 
     toggleFilter(string) {
@@ -32,6 +42,13 @@ export class InvoicesComponent implements OnInit {
         } else {
             this.filterString = string;
         }
+    }
+
+    research() {
+        this.invoiceService.fetchInvoices(this.searchForm.value)
+            .subscribe( (invoices) => {
+                this.invoices = invoices;
+            });
     }
 
 }
