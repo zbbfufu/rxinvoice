b11c8d4a1c96a5825f7c06fbb478b7e7734778d6
cDaffe <celine.daffe@4sh.fr>
2018-02-05 17:56:16 +0100

    [front] add fetchInvoices call by status

--- ui/src/app/pages/dashboard/dashboard.component.ts
+++ ui/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {InvoiceModel} from '../../models/invoice.model';
+import {InvoiceService} from '../../common/services/invoice.service';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+    preparedInvoices: InvoiceModel[];
+    validatedInvoices: InvoiceModel[];
+    sentInvoices: InvoiceModel[];
+    reviveInvoices: InvoiceModel[];
+    isPending = true;
+
+  constructor(private invoiceService: InvoiceService) { }
 
   ngOnInit() {
+    this.invoiceService.fetchInvoices({status: 'DRAFT', startDate: '2018-01-01'})
+        .subscribe(invoices => {
+            this.preparedInvoices = invoices;
+            this.isPending = false;
+        });
+    this.invoiceService.fetchInvoices({status: 'WAITING_VALIDATION', startDate: '2018-01-01'})
+        .subscribe(invoices => this.validatedInvoices = invoices);
+    this.invoiceService.fetchInvoices({status: 'SENT', startDate: '2018-01-01'})
+        .subscribe(invoices => this.sentInvoices = invoices);
+    this.invoiceService.fetchInvoices({status: 'LATE', startDate: '2018-01-01'})
+        .subscribe(invoices => this.reviveInvoices = invoices);
   }
-
 }
