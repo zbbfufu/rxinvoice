fb29fbfa02a3dc25f8220fa070af1000de1edd91
cDaffe <celine.daffe@4sh.fr>
2018-02-06 17:24:08 +0100

    [front] Add user and getMomentFromNow

--- ui/src/app/common/components/app-header/app-header.component.ts
+++ ui/src/app/common/components/app-header/app-header.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
+import {User} from '../../../models/user.model';
+import {ActivityModel} from '../../../models/activity.model';
+import {ActivityService} from '../../services/activity.service';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.scss']
+    selector: 'app-header',
+    templateUrl: './app-header.component.html',
+    styleUrls: ['./app-header.component.scss']
 })
 export class AppHeaderComponent implements OnInit {
 
     activityMenuOpen = false;
+    user: User;
+    activities: ActivityModel[];
 
-  constructor() { }
+    constructor(private authenticationService: AuthenticationService,
+                private activityService: ActivityService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.user = this.authenticationService.current();
+        this.activityService.fetchActivities()
+            .subscribe(activities => this.activities = activities);
+    }
+
+    public getMomentFromNow(date) {
+        return moment(date).fromNow();
+    }
 
 }
