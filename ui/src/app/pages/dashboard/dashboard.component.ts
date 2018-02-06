441b74c229fb3fe6266383052dedb0203c22ced7
cDaffe <celine.daffe@4sh.fr>
2018-02-06 11:15:53 +0100

    [front] Add amount calculation and numberOfElements input

--- ui/src/app/pages/dashboard/dashboard.component.ts
+++ ui/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {InvoiceModel} from '../../models/invoice.model';
 import {InvoiceService} from '../../common/services/invoice.service';
+import {isNumber} from 'util';
 
 @Component({
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
@@ -14,19 +15,31 @@ export class DashboardComponent implements OnInit {
     reviveInvoices: InvoiceModel[];
     isPending = true;
 
-  constructor(private invoiceService: InvoiceService) { }
+    constructor(private invoiceService: InvoiceService) {
+    }
 
-  ngOnInit() {
-    this.invoiceService.fetchInvoices({status: 'DRAFT', startDate: '2018-01-01'})
-        .subscribe(invoices => {
-            this.preparedInvoices = invoices;
-            this.isPending = false;
-        });
-    this.invoiceService.fetchInvoices({status: 'WAITING_VALIDATION', startDate: '2018-01-01'})
-        .subscribe(invoices => this.validatedInvoices = invoices);
-    this.invoiceService.fetchInvoices({status: 'SENT', startDate: '2018-01-01'})
-        .subscribe(invoices => this.sentInvoices = invoices);
-    this.invoiceService.fetchInvoices({status: 'LATE', startDate: '2018-01-01'})
-        .subscribe(invoices => this.reviveInvoices = invoices);
-  }
+    ngOnInit() {
+        this.invoiceService.fetchInvoices({status: 'DRAFT', startDate: '2018-01-01'})
+            .subscribe(invoices => {
+                this.preparedInvoices = invoices;
+                this.isPending = false;
+            });
+        this.invoiceService.fetchInvoices({status: 'WAITING_VALIDATION', startDate: '2018-01-01'})
+            .subscribe(invoices => this.validatedInvoices = invoices);
+        this.invoiceService.fetchInvoices({status: 'SENT', startDate: '2018-01-01'})
+            .subscribe(invoices => this.sentInvoices = invoices);
+        this.invoiceService.fetchInvoices({status: 'LATE', startDate: '2018-01-01'})
+            .subscribe(invoices => this.reviveInvoices = invoices);
+    }
+
+    getAmount(invoices: InvoiceModel[]) {
+        if (invoices) {
+            const number = invoices
+                .filter(invoice => isNumber(invoice.grossAmount))
+                .map(invoice => invoice.grossAmount)
+                .reduce((a, b) => a + b);
+            console.log(number);
+            return number;
+        }
+    }
 }
