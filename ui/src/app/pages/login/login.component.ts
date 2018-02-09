f3f0b9c9a93c048e1dda5d1e162ede2d700d62af
cDaffe <celine.daffe@4sh.fr>
2018-02-09 16:25:13 +0100

    [front] Improve alert display on login

--- ui/src/app/pages/login/login.component.ts
+++ ui/src/app/pages/login/login.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../../common/services/authentication.service';
 import {SweetAlertService} from '../../common/services/sweetAlert.service';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
     constructor(private router: Router,
                 private authService: AuthenticationService,
-                private alertService: SweetAlertService,) {}
+                private alertService: SweetAlertService) {
+    }
 
     ngOnInit() {
 
     }
+
     public login(value) {
         this.authService.authenticate(value)
             .subscribe((user) =>  {
@@ -26,7 +28,8 @@ export class LoginComponent implements OnInit {
                     this.alertService.error({
                         text: 'login.alert.check',
                         title: 'login.alert.fail',
-                        customClass: 'swal2-for-login'
+                        customClass: 'swal2-for-login',
+                        timer: 3000
                     });
                 }
             });
