04a53c2069f0780cb3a1c246f0fd9c28a2b69261
Xavier Hanin <xavier.hanin@4sh.fr>
2020-01-14 14:18:45 +0100

    chore: add docker file and script to build and publish to GCR

---
+++ srv/docker-build-push.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+CONTAINER=core
+
+if [[ -z "$PROJECT_ID" ]]; then
+    echo "Must provide GCP project id as PROJECT_ID variable in environment" 1>&2
+    exit 1
+fi
+
+POM_ARTIFACT=`xmllint --nocdata  --xpath "/*[name()='project']/*[name()='artifactId']/text()" pom.xml | awk '{$1=$1};1'`
+POM_VERSION=`xmllint --nocdata  --xpath "/*[name()='project']/*[name()='version']/text()" pom.xml | awk '{$1=$1};1'`
+VERSION=${1:-$POM_VERSION}
+ARTIFACT=${1:-$POM_ARTIFACT}
+IMAGE_VERSION_SUFFIX=${2:-}
+
+if [[ -z "$VERSION" ]]; then
+    echo "Must provide version as parameter" 1>&2
+    exit 1
+fi
+
+ARTIFACT_FILE=target/$ARTIFACT-$VERSION.war
+if [ ! -f "$ARTIFACT_FILE" ]; then
+    echo "$ARTIFACT_FILE does not exist"
+    exit 1
+fi
+
+IMAGE=eu.gcr.io/$PROJECT_ID/$CONTAINER:$VERSION$IMAGE_VERSION_SUFFIX
+
+echo building $IMAGE
+
+rm target/ROOT.war
+cp $ARTIFACT_FILE target/ROOT.war
+docker build -t $IMAGE . && docker tag $IMAGE gcr.io/$PROJECT_ID/$CONTAINER:latest && docker push gcr.io/$PROJECT_ID/$CONTAINER
