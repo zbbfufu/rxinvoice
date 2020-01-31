5268fe0a352393085de2c8c7d786d4d760433653
Julien Furgerot <julien.furgerot@gmail.com>
2020-01-31 16:38:01 +0100

    docker builder with xmllint

--- ci/cloud-build/docker-build-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IMAGE=eu.gcr.io/quatreapp/mvn-npm-builder
-TAG=0.1
-
-docker build -t $IMAGE:$TAG -f Dockerfile.mvn-npm-builder .
-docker tag $IMAGE:$TAG $IMAGE:latest 
-docker push $IMAGE:$TAG
-docker push $IMAGE:latest
