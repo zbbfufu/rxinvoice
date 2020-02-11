0794b2ab411b962f19ff680dd2eca7bf77413919
carmarolli <christophe.armarolli@4sh.fr>
2020-02-11 17:53:27 +0100

    RXINVO-164 Workflow : ajout des status UNPAID et PAYMENT_SCHEDULED

--- srv/src/main/java/rxinvoice/domain/invoice/Status.java
+++ srv/src/main/java/rxinvoice/domain/invoice/Status.java
@@ -1,5 +1,14 @@
 package rxinvoice.domain.invoice;
 
 public enum Status {
-    DRAFT, READY, SENT, LATE, PAID, CANCELLED, WAITING_VALIDATION, VALIDATED
+    DRAFT,
+    READY,
+    SENT,
+    LATE,
+    PAID,
+    CANCELLED,
+    WAITING_VALIDATION,
+    VALIDATED,
+    UNPAID,
+    PAYMENT_SCHEDULED
 }
